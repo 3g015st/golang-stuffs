4f1c87dbb344e6d7b69e3bb10d29ef67259b5930
3g015st <markbenedict98@gmail.com>
2023-06-03 19:16:02 +0800

    Lambda server

--- devops/aws-lambda-deployment/cmd/main.go
+++ devops/aws-lambda-deployment/cmd/main.go
@@ -13,8 +13,11 @@ func main() {
 	if err != nil {
 	  log.Fatal("Error loading .env file")
 	}
+
+	lambdaServer := os.Getenv("_LAMBDA_SERVER_PORT")
 	
 	appName := os.Getenv("APP_NAME")
     fmt.Println("AWS Lambda Deployment Sample")
 	fmt.Println("App Name : ", appName)
+	fmt.Println("Lamda Server : ", lambdaServer)
 }
