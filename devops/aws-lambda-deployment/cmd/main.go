e22c45ea434b65f0dca4ff2f518662ce3366fd99
3g015st <markbenedict98@gmail.com>
2023-06-03 19:42:13 +0800

    Added event bridge sampler

--- devops/aws-lambda-deployment/cmd/main.go
+++ devops/aws-lambda-deployment/cmd/main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/joho/godotenv"
 )
+
+type EventBridgeMessage struct {
+	Schedule string
+}
+
+func handler(ctx context.Context, event events.CloudWatchEvent) error {
+	// Parse the event detail into a map[string]interface{}
+	var msg EventBridgeMessage
+	if err := json.Unmarshal(event.Detail, &msg); err != nil {
+		fmt.Println("Unmarshal error", err.Error())
+	}
+
+	fmt.Printf("Received Event Bridge Message: %+v\n", msg.Schedule)
+
+	return nil
+}
 
 func main() {
 	err := godotenv.Load()
@@ -20,4 +40,6 @@ func main() {
     fmt.Println("AWS Lambda Deployment Sample")
 	fmt.Println("App Name : ", appName)
 	fmt.Println("Lamda Server : ", lambdaServer)
+
+	lambda.Start(handler)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module golang-stuff
 
 go 1.20
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/aws/aws-lambda-go v1.41.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
