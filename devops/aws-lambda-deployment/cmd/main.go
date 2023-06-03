62865d79f2b11eb15221a643136e2241adf01b0b
3g015st <markbenedict98@gmail.com>
2023-06-03 17:21:16 +0800

    Init commit

---
+++ devops/aws-lambda-deployment/cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+	
+	appName := os.Getenv("APP_NAME")
+    fmt.Println("AWS Lambda Deployment Sample")
+	fmt.Println("App Name : ", appName)
+}
